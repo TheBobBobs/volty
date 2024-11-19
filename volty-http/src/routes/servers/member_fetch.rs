db2dad36cb5745f697884cd06c8170b64dffe289
Bob Bobs <bobthebobs@protonmail.com>
2024-11-18 21:07:01 -0700

    add fetch member with roles

--- volty-http/src/routes/servers/member_fetch.rs
+++ volty-http/src/routes/servers/member_fetch.rs
@@ -1,7 +1,23 @@
+use std::collections::HashMap;
+
 use reqwest::Method;
-use volty_types::servers::server_member::Member;
+use serde::{Deserialize, Serialize};
+use volty_types::servers::{server::Role, server_member::Member};
 
 use crate::{error::HttpError, Http};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+enum MemberResponse {
+    Member(Member),
+    MemberWithRoles(MemberWithRoles),
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct MemberWithRoles {
+    member: Member,
+    roles: HashMap<String, Role>,
+}
 
 impl Http {
     pub async fn fetch_member(
@@ -11,6 +27,26 @@ impl Http {
     ) -> Result<Member, HttpError> {
         let path = format!("servers/{server_id}/members/{user_id}");
         let request = self.request(Method::GET, &path)?;
-        self.send_request(request).await
+        let result: Result<MemberResponse, _> = self.send_request(request).await;
+        result.map(|m| match m {
+            MemberResponse::Member(m) => m,
+            MemberResponse::MemberWithRoles(MemberWithRoles { member, roles: _ }) => member,
+        })
+    }
+
+    pub async fn fetch_member_with_roles(
+        &self,
+        server_id: impl std::fmt::Display,
+        user_id: impl std::fmt::Display,
+    ) -> Result<MemberWithRoles, HttpError> {
+        let path = format!("servers/{server_id}/members/{user_id}?roles=true");
+        let request = self.request(Method::GET, &path)?;
+        let result: Result<MemberResponse, _> = self.send_request(request).await;
+        result.map(|m| {
+            let MemberResponse::MemberWithRoles(data) = m else {
+                panic!();
+            };
+            data
+        })
     }
 }
